5fccf09402dd97ed13ac8876b0b1aefd45a0e530
ritchies <stephen.ritchie@cfpb.gov>
2013-06-28 16:45:30 -0400

    Added the minimal python packaging

---
+++ pycabara/__init__.py
@@ -0,0 +1,6 @@
+__author__ = 'ruthlesshelp'
+
+
+def about():
+    return (r'Python implementation of Capybara, an acceptance test framework for web applications.'
+            r'Pycabara source https://github.com/ruthlesshelp/pycabara')
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pycabara',
+      version='0.1',
+      description='Python implementation of Capybara, an acceptance test framework for web applications',
+      url='http://github.com/ruthlesshelp/pycabara',
+      author='Stephen Ritchie',
+      author_email='ruthlesshelp@gmail.com',
+      license='MIT',
+      packages=['pycabara'],
+      zip_safe=False)
