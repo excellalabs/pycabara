4d52aada4faa409047037a5fcde239041210a407
ritchies <stephen.ritchie@cfpb.gov>
2013-06-28 17:24:28 -0400

    Minor refactoring

--- pycabara/__init__.py
+++ pycabara/__init__.py
@@ -1,6 +1,1 @@
 __author__ = 'ruthlesshelp'
-
-
-def about():
-    return (r'Python implementation of Capybara, an acceptance test framework for web applications.'
-            r'Pycabara source https://github.com/ruthlesshelp/pycabara')
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pycabara',
-      version='0.1',
+      version='0.2',
       description='Python implementation of Capybara, an acceptance test framework for web applications',
       url='http://github.com/ruthlesshelp/pycabara',
       author='Stephen Ritchie',
